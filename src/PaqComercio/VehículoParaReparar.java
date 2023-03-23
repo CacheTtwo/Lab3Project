5f26f0f05659f682c862aff5c576ed7347092a8f
CacheTtwo <manuelalejandrotorres2905@outlook.com>
2023-03-23 17:23:37 +0100

    Clones y ConstructorCopy

--- src/PaqComercio/VehículoParaReparar.java
+++ src/PaqComercio/VehículoParaReparar.java
@@ -1,4 +1,43 @@
 package PaqComercio;
 
-public class VehículoParaReparar {
+public class VehículoParaReparar extends Vehículo
+{
+    private String Avería;
+    private boolean reparado;
+    private int prioridad;
+
+    public VehículoParaReparar(String marca, String modelo, String matrícula, String avería, boolean reparado, int prioridad) {
+        super(marca, modelo, matrícula);
+        Avería = avería;
+        this.reparado = reparado;
+        this.prioridad = prioridad;
+    }
+
+    public String getAvería() {
+        return Avería;
+    }
+
+    public boolean isReparado() {
+        return reparado;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setAvería(String avería) {
+        Avería = avería;
+    }
+
+    public void setReparado(boolean reparado) {
+        this.reparado = reparado;
+    }
+
+    public void setPrioridad(int prioridad) {
+        if (prioridad>0&&prioridad<3){
+            this.prioridad = prioridad;}
+        else this.prioridad= 1;
+
+    }
+
 }
